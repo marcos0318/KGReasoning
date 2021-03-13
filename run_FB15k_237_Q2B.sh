330669405632983b3c100e9418236550badb1da0
bijetion <jbai@connect.ust.hk>
2021-03-13 16:57:52 +0800

    add 10times script

--- run_FB15k_237_Q2B.sh
+++ run_FB15k_237_Q2B.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
-  --data_path data/FB15k-237-betae -n 128 -b 512 -d 400 -g 24 \
-  -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo box --valid_steps 15000 \
+  --data_path data/FB15k-237-betae -n 128 -b 512 -d 4000 -g 24 \
+  -lr 0.0001 --max_steps 4500001 --cpu_num 1 --geo box --valid_steps 15000 \
   -boxm "(none,0.02)" --tasks "1p.2p.3p.2i.3i.ip.pi.2u.up"
