f1bfc90a00b062dc71c064e9795b3ea4b55f558a
bijetion <jbai@connect.ust.hk>
2021-03-13 17:05:05 +0800

    reduce batch size

--- run_FB15k_237_Beta.sh
+++ run_FB15k_237_Beta.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=1 python main.py --cuda --do_train --do_valid --do_test \
-  --data_path data/FB15k-237-betae -n 128 -b 256 -d 4000 -g 60 \
+  --data_path data/FB15k-237-betae -n 128 -b 64 -d 4000 -g 60 \
   -lr 0.0001 --max_steps 9000001 --cpu_num 1 --geo beta --valid_steps 15000 \
   -betam "(1600,2)"
